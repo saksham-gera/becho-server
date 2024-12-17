82405394e06aa12605d33ddceb9f351a67a5bf6b
ss <kotadiyasaiyam123@gmail.com>
2024-12-17 18:49:04 +0530

    bcrypt js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import bodyParser from "body-parser";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import db from "./db.js";
 
 const app = express();
