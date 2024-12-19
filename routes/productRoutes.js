01cdca077b0f4eaba00c9f0c1d21edebd19629e9
ss <kotadiyasaiyam123@gmail.com>
2024-12-20 00:39:15 +0530

    product routes

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getProducts } from "../controllers/productController.js";
+import { getProducts,insertProduct } from "../controllers/productController.js";
 
 const router = express.Router();
 
