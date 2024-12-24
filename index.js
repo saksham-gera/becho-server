93904c0e562bf17fa2f0bda9f1b6f9a6aed143a2
Saksham Gera <sakshamgera1456@gmail.com>
2024-12-25 02:31:11 +0530

    Commission Routes Added

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 import express from "express";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
+import cors from "cors";
 import authRoutes from "./routes/authRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import wishlistRoutes from "./routes/wishlistRoutes.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
-import reelsRoutes from "./routes/reelRoutes.js"
-import commissionsRoutes from "./routes/commissionRoutes.js"
+import reelsRoutes from "./routes/reelRoutes.js";
+import commissionsRoutes from "./routes/commissionRoutes.js";
+
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Enable CORS
+app.use(cors());
+
+// Body Parser Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
@@ -20,12 +26,12 @@ app.use(bodyParser.json());
 app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
 app.use("/products", productRoutes);
-app.use('/wishlist', wishlistRoutes);
-app.use('/category', categoryRoutes);
-app.use('/reels', reelsRoutes);
-app.use('/commissions', commissionsRoutes);
+app.use("/wishlist", wishlistRoutes);
+app.use("/category", categoryRoutes);
+app.use("/reels", reelsRoutes);
+app.use("/commissions", commissionsRoutes);
 
-// Global error handler
+// Global Error Handler
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({
@@ -35,6 +41,7 @@ app.use((err, req, res, next) => {
   });
 });
 
+// Start Server
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
