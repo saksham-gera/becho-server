2eadf29e18affb61da7c719658ab4c0d22bd135f
Saksham Gera <sakshamgera1456@gmail.com>
2024-12-28 19:33:23 +0530

    Category updated

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -1,9 +1,10 @@
 import express from "express"
-import {getCategories, addCategory, deleteCategory, updateCategory} from "../controllers/categoryController.js";
+import {getCategories, addCategory, deleteCategory, updateCategory, getCategoryById} from "../controllers/categoryController.js";
 
 const router = express.Router();
 
 router.get('/', getCategories);
+router.get('/:id', getCategoryById);
 router.post('/', addCategory);
 router.post('/delete', deleteCategory);
 router.post('/update', updateCategory);
